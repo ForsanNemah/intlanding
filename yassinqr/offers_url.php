eff586605c8022ecfe1f45d237268c11e1e93dd5
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-03-29 12:34:42 +0300

    ss

---
+++ yassinqr/offers_url.php
@@ -0,0 +1,5 @@
+<?php
+// Redirect to the WhatsApp URL
+header("Location: https://api.whatsapp.com/send/?phone=966567959648&text=%D8%A7%D8%B4%D8%AA%D8%B1%D8%A7%D9%83+%D9%81%D9%8A+%D8%B9%D8%B1%D9%88%D8%B6+%D8%A8%D9%87%D8%A7%D8%B1%D8%A7%D8%AA+%D8%A7%D9%84%D9%8A%D8%A7%D8%B3%D9%8A%D9%86&type=phone_number&app_absent=0");
+exit;
+?>
