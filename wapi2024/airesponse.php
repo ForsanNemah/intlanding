ef53c2479631df03e77988740c9d465d6af9b8a9
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-04-19 16:04:34 -0700

    ss

--- wapi2024/airesponse.php
+++ wapi2024/airesponse.php
@@ -97,7 +97,7 @@ if($is_me !== "1" && $chat_id !== $chat_gpt_id) {
 //$result = send_with_wapi($token, $profile_id,'966568430828@c.us', $message." my msg to ".$chat_id );
 
 
-$result = send_with_wapi($token, $profile_id,'966568430828@c.us', $message."  ".$is_me );
+//$result = send_with_wapi($token, $profile_id,'966568430828@c.us', $message."  ".$is_me );
 
 
 
