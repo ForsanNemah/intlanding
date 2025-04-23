b109114ce5669df1c8284eaab245e03fbde96dd7
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-04-22 17:23:30 -0700

    ss

--- intshar_team/forsan.php
+++ intshar_team/forsan.php
@@ -1,3 +1,19 @@
 <?php
-echo "forsan";
-?>
+// اسم الملف
+$filename = 'int_contcat.vcf';
+
+// تأكد أن الملف موجود
+if (file_exists($filename)) {
+    // إجبار المتصفح على تحميل الملف
+    header('Content-Description: File Transfer');
+    header('Content-Type: text/vcard');
+    header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($filename));
+    readfile($filename);
+    exit;
+} else {
+    echo "الملف غير موجود.";
+}
