c11254296d762e0fd28333d08e72b53dfd3b5792
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-05-11 16:35:14 -0700

    ss

--- decon/head.php
+++ decon/head.php
@@ -1,5 +1,16 @@
 <head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WQTYNVCR0L"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WQTYNVCR0L');
+</script>
+
+
 
 
 <?php
