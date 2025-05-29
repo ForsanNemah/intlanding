486c24b870e9197c0983dc6fd803315915bc138a
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-05-28 21:04:47 -0700

    almosa

---
+++ almosa/single_image.php
@@ -0,0 +1,5 @@
+<div class="container h-100 d-flex justify-content-center align-items-center">
+    <div class="text-center">
+      <img src="single_image/1.jpg" class="img-fluid" alt="Centered Image" style="max-width: 100%; height: auto;">
+    </div>
+  </div>
