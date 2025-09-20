5839fefc68673287f559bcc445d3d6379c9ada5f
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-09-20 17:16:41 +0300

    sss

---
+++ zomord/hero.php
@@ -0,0 +1,46 @@
+<section id="hero" class="hero-section position-relative text-white d-flex align-items-center" style="min-height: 100vh;">
+    <!-- خلفية بصورة -->
+    <div class="hero-bg position-absolute top-0 start-0 w-100 h-100" 
+         style="background: url('baner/baner.png') center center/cover no-repeat; z-index:-2;">
+    </div>
+
+    <!-- طبقة شفافة -->
+   <div class="hero-bg position-absolute top-0 start-0 w-100 h-100"
+     style="background: url('./baner/baner.png') center center/cover no-repeat;">
+</div>
+    
+    <div class="container text-center " style="padding-top:0px";>
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="hero-content">
+                    <h1 class="hero-title display-4 fw-bold mb-3"> 
+                        
+                   
+                <?php echo $ad_main_title; ?>
+                
+                </h1>
+
+                    <p class="hero-subtitle lead mb-4">
+                        استمتع بأفضل العروض الطبية والتجميلية بمناسبة اليوم الوطني السعودي 🇸🇦<br>
+                        خدمات طبية متميزة بأسعار ما تتفوت  !
+                    </p>
+                    
+                    <div class="d-flex flex-wrap justify-content-center gap-3">
+                        <!-- زر التسجيل -->
+                        <a href="#registrationForm" class="btn btn-primary btn-lg px-4 shadow-sm rounded-pill">
+                            سجّل الآن
+                        </a>
+                        <!-- زر واتساب -->
+                        <a href="https://wa.me/<?php echo $w_app_number; ?>" target="_blank" class="btn btn-success btn-lg px-4 shadow-sm rounded-pill">
+                            <i class="bi bi-whatsapp me-2"></i> حجز موعد عبر واتساب
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+ 
+
